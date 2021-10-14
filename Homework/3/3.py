3e268fcb7971b872c322d2e3370e09aac3085e7d
Azamat Bayramov <bayramov.azamat04@gmail.com>
2021-10-14 16:32:09 +0500

    Problem 3-3 solved

---
+++ Homework/3/3.py
@@ -0,0 +1,49 @@
+def heapify(heap, i=0, finish=None, start=False):
+    if finish is None:
+        finish = len(heap)
+
+    if not i * 2 + 1 < finish:
+        return
+
+    if not i * 2 + 2 < finish:
+        if heap[i] < heap[i * 2 + 1]:
+            heap[i], heap[i * 2 + 1] = heap[i * 2 + 1], heap[i]
+        return
+
+    if start:
+        heapify(heap, i * 2 + 1, finish, start=start)
+        heapify(heap, i * 2 + 2, finish, start=start)
+
+    if heap[i] < heap[i * 2 + 1] or heap[i] < heap[i * 2 + 2]:
+        if heap[i * 2 + 1] < heap[i * 2 + 2]:
+            heap[i], heap[i * 2 + 2] = heap[i * 2 + 2], heap[i]
+            heapify(heap, i * 2 + 2, finish)
+        else:
+            heap[i], heap[i * 2 + 1] = heap[i * 2 + 1], heap[i]
+            heapify(heap, i * 2 + 1, finish)
+
+
+def heap_sort(a):
+    n = len(a)
+
+    heapify(a, start=True)
+
+    for i in range(n):
+        j = n - i
+        heapify(a, finish=j)
+        a[0], a[j - 1] = a[j - 1], a[0]
+
+    return
+
+
+if __name__ == '__main__':
+    n = int(input())
+
+    lst = []
+
+    for i in range(n):
+        lst.append(int(input()))
+
+    heap_sort(lst)
+
+    print(lst)
